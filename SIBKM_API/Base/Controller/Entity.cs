88ba2589f9121598fd9231144f3067bdc82ec55b
Selvianisa Cahya Mukti <55840976+L200190170@users.noreply.github.com>
2022-11-24 00:18:21 +0700

    Initial Commit

---
+++ SIBKM_API/Base/Controller/Entity.cs
@@ -0,0 +1,6 @@
+﻿namespace SIBKM_API.Base.Controller
+{
+    public class Entity
+    {
+    }
+}
---
+++ SIBKM_API/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIBKM_API.Models
+{
+    public class Role
+    {
+        [Key]
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+    }
+}
